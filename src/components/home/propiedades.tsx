229cc9954e2919814a941427f297d98088b77693
commodore_64 <cesarml2002@hotmail.com>
2024-02-28 13:22:48 -0600

    Dialog propiedades detalles v1.0

--- src/components/home/propiedades.tsx
+++ src/components/home/propiedades.tsx
@@ -1,4 +1,11 @@
 import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
   Dialog,
   DialogContent,
   DialogDescription,
@@ -6,9 +13,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+
 import { FC, useEffect, useState } from "react";
 import { FaBath, FaBed, FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
 import { IoIosMore } from "react-icons/io";
+import { RxRulerHorizontal } from "react-icons/rx";
+import { TbResize } from "react-icons/tb";
 
 // Utils
 import {
@@ -140,8 +150,67 @@ const Propiedades: FC = () => {
                   <DialogContent>
                     <DialogHeader>
                       <DialogTitle className="m-3">{item.title}</DialogTitle>
+                      <p className="m-3 text-gray-500">
+                        {propertyDetails.property_type} en{" "}
+                        {propertyDetails.location.name}
+                      </p>
+                      {propertyDetails && (
+                        <Carousel className="w-full max-w-lg">
+                          <CarouselContent>
+                            {propertyDetails.property_images.map(
+                              (image: any, index: number) => (
+                                <CarouselItem key={index}>
+                                  <div className="p-1 h-96">
+                                    <img
+                                      src={image.url}
+                                      alt={`Image ${index}`}
+                                      className="w-full h-full object-cover rounded-lg"
+                                    />
+                                  </div>
+                                </CarouselItem>
+                              )
+                            )}
+                          </CarouselContent>
+                          <CarouselPrevious />
+                          <CarouselNext />
+                        </Carousel>
+                      )}
+                      <div className="flex flex-row">
+                        <div className="flex items-center mr-4">
+                          <FaBed className="w-5 h-4 mr-2" />
+                          <span>{propertyDetails.bedrooms || 0}</span>
+                        </div>
+                        <div className="flex items-center">
+                          <FaBath className="w-4 h-4 mr-2" />
+                          <span>{propertyDetails.bathrooms || 0}</span>
+                        </div>
+                      </div>
+                      <div className="flex items-center mr-4">
+                        <TbResize className="w-5 h-4 mr-2" />
+                        <span>
+                          {propertyDetails.lot_size?.toLocaleString() || 0} m²
+                          de Terreno
+                        </span>
+                      </div>
+                      <div className="flex items-center mr-4">
+                        <RxRulerHorizontal className="w-5 h-4 mr-2" />
+                        <span>
+                          {propertyDetails.lot_width?.toLocaleString() || 0} m
+                        </span>
+                        <div className="flex items-center mr-4">
+                          <RxRulerHorizontal className="w-5 h-4 mx-2 transform rotate-90" />
+                          <span>
+                            {propertyDetails.lot_length?.toLocaleString() || 0}{" "}
+                            m
+                          </span>
+                        </div>
+                      </div>
+
                       {propertyDetails && (
                         <DialogDescription>
+                          <p className="font-bold text-base text-gray-800 py-2">
+                            Descripción
+                          </p>
                           {propertyDetails.description}
                         </DialogDescription>
                       )}
